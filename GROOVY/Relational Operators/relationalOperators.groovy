6dc052c5b9d74355e945df63cb88e1e011392025
amanmehara <amanmehara@hotmail.com>
2017-09-24 21:32:28 +0530

    Add 1 Project
    GROOVY ## Relational Operators

---
+++ GROOVY/Relational Operators/relationalOperators.groovy
@@ -0,0 +1,24 @@
+/**
+ * Created by @amanmehara on 24-09-2017.
+ */
+
+/*
+
+ == : equal
+ != : different
+  < : less than
+ <= : less than or equal
+  > : greater than
+ >= : greater than or equal
+
+ */
+
+assert 1 + 2 == 3 // equal
+assert 3 != 4 // different
+
+assert -2 < 3 // less than
+assert 2 <= 2 // less than or equal
+assert 3 <= 4 // less than or equal
+
+assert 5 > 1 // greater than
+assert 5 >= -2 // greater than or equal
