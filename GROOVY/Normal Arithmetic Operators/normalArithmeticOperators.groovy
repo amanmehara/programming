441bbe5fb9464b95a6ae27cb463860692ab9226b
amanmehara <amanmehara@hotmail.com>
2017-09-26 21:53:43 +0530

    Add 1 Project
    GROOVY ## Normal Arithmetic Operators

---
+++ GROOVY/Normal Arithmetic Operators/normalArithmeticOperators.groovy
@@ -0,0 +1,23 @@
+/**
+ * Created by @amanmehara on 24-09-2017.
+ */
+
+/*
+
+  + : addition
+  - : subtraction
+  * : multiplication
+  / : division
+  % : remainder
+ ** : power
+
+ */
+
+assert  1  + 2 == 3 // addition
+assert  4  - 3 == 1 // subtraction
+assert  3  * 5 == 15 // multiplication
+assert  3  / 2 == 1.5 // division
+assert 10  % 3 == 1 // remainder
+assert  2 ** 3 == 8 // power
+
+// Use intdiv() for integer division
