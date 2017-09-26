9291cbc2a6ac7a59fbfd1c6e991ebb6699d4cb74
amanmehara <amanmehara@hotmail.com>
2017-09-26 21:30:46 +0530

    Add 1 Projects
    GROOVY ## Logical Operators

---
+++ GROOVY/Logical Operators/logicalOperators.groovy
@@ -0,0 +1,15 @@
+/**
+ * Created by @amanmehara on 24-09-2017.
+ */
+
+/*
+
+ && : logical "and"
+ || : logical "or"
+  ! : logical "not"
+
+ */
+
+assert !false // "not" false is true
+assert true && true // true "and" true is true
+assert true || false // true "or" false is true
