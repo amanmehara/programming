06375047dc6672900ed86798b0fa9a95aed69feb
maulikchevli <maulikchevli@gmail.com>
2017-10-14 13:29:31 +0530

    Add 1 project

---
+++ PYTHON/While Statement/whileStatement.py
@@ -0,0 +1,12 @@
+name=''
+
+#infinte loop
+while True:
+    print('Enter your name')
+    name=input()
+    
+    #enter 'your name' to terminate the loop
+    if name=='your name':
+        break
+    
+print('Thank You')
