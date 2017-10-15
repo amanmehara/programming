5bfa2a403b9849c55faa42064a8d937ae86291b4
amanmehara <amanmehara@hotmail.com>
2017-10-16 00:32:46 +0530

    Add 1 Project
    JAVA ## Recursion (Fibonacci Number)

---
+++ JAVA/Recursion (Fibonacci Number)/RecursiveFibonacciNumber.java
@@ -0,0 +1,30 @@
+/*
+
+ Copyright 2017 Aman Mehara
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ */
+
+public class RecursiveFibonacciNumber {
+
+    public int fibonacciNumber(int number) {
+        int fibonacciNumber = recurse(Math.abs(number));
+        return number < 0 ? (int) Math.pow(-1, number + 1) * fibonacciNumber : fibonacciNumber;
+    }
+
+    private int recurse(int number) {
+        return number > 1 ? recurse(number - 1) + recurse(number - 2) : number;
+    }
+
+}
