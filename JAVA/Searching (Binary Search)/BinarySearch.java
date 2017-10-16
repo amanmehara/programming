58c74941231c3f48991e158e0ba451cc64af1adb
amanmehara <amanmehara@hotmail.com>
2017-10-17 01:04:05 +0530

    Add 2 JAVA Projects

---
+++ JAVA/Searching (Binary Search)/BinarySearch.java
@@ -0,0 +1,46 @@
+/*
+
+ Copyright 2017 Aman Mehara
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ */
+
+import java.util.Optional;
+
+public class BinarySearch {
+
+    public Optional<Integer> search(Integer[] array, Integer key) {
+
+        int start = 0;
+        int end = array.length - 1;
+
+        while (start <= end) {
+
+            int middle = start + (end - start) / 2;
+
+            if (key < array[middle]) {
+                end = middle - 1;
+            } else if (key.equals(array[middle])) {
+                return Optional.of(middle);
+            } else {
+                start = middle + 1;
+            }
+
+        }
+
+        return Optional.empty();
+
+    }
+
+}
