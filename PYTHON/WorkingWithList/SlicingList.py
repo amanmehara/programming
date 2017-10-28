c62990a0c1b9f26c4a56e1603fb4997e63d95dab
Rajput Vishwas Kumar Shankarbhai <Rajput_Vishwas@ad.infosys.com>
2017-10-28 14:12:50 +0530

    added two python programs

---
+++ PYTHON/WorkingWithList/SlicingList.py
@@ -0,0 +1,13 @@
+#Working with list
+my_list = ['p','r','o','g','r','a','m','i','z']
+# elements 3rd to 5th
+print(my_list[2:5])
+
+# elements beginning to 4th
+print(my_list[:-5])
+
+# elements 6th to end
+print(my_list[5:])
+
+# elements beginning to end
+print(my_list[:])
