6518687f36190ea1a100269844d3b42489ea3e0c
krazune <krazune@gmail.com>
2017-10-31 21:58:59 +0000

    Arithmetic Progression added.

---
+++ CPP/Arithmetic Progression/ArithmeticProgression.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+double arithmeticProgression(double firstTerm, double commonDifference, int numberOfTerms)
+{
+    return firstTerm + numberOfTerms * commonDifference;
+}
+
+int main()
+{
+    double firstTerm = 0;
+    double commonDifference = 0;
+    int numberOfTerms = 0;
+
+    std::cout << "First term: ";
+    std::cin >> firstTerm;
+
+    std::cout << "Common difference: ";
+    std::cin >> commonDifference;
+
+    std::cout << "Number of terms: ";
+    std::cin >> numberOfTerms;
+
+    for(int termIndex = 0; termIndex < numberOfTerms; termIndex++)
+    {
+        std::cout << arithmeticProgression(firstTerm, commonDifference, termIndex) << std::endl;
+    }
+
+    return 0;
+}
