dcb6137a254bfdeb4a8b3692950d56a08f227c7a
Aman Mehara <amanmehara@hotmail.com>
2018-01-30 23:51:00 +0530

    Add 1 Project
    C++ ## Graph (Directed)

---
+++ CPP/Graph (Directed)/directed_graph.cpp
@@ -0,0 +1,74 @@
+/*
+
+ Copyright 2018 Aman Mehara
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+
+ */
+
+#include <algorithm>
+#include <stdexcept>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+
+using namespace std;
+
+class DirectedGraph {
+
+	unordered_map<int, unordered_set<int>> adjacency_list_;
+
+public:
+
+	bool Adjacent(int source_vertex, int target_vertex) {
+		return adjacency_list_.count(source_vertex) && adjacency_list_.find(source_vertex)->second.count(target_vertex);
+	}
+
+	unordered_set<int> Neighbours(int vertex) {
+		if (adjacency_list_.count(vertex)) {
+			return adjacency_list_.find(vertex)->second;
+		}
+		else {
+			throw invalid_argument("Vertex not found in the Graph.");
+		}
+	}
+
+	void AddVertex(int vertex) {
+		adjacency_list_.insert(make_pair<>(vertex, unordered_set<int>()));
+	}
+
+	void RemoveVertex(int vertex) {
+		if (adjacency_list_.count(vertex)) {
+			adjacency_list_.erase(vertex);
+			for (unordered_map<int, unordered_set<int>>::iterator it = adjacency_list_.begin(); it != adjacency_list_.end(); it++) {
+				it->second.erase(vertex);
+			}
+		}
+	}
+
+	void AddEdge(int source_vertex, int target_vertex) {
+		if (adjacency_list_.count(source_vertex) && adjacency_list_.count(target_vertex)) {
+			adjacency_list_.find(source_vertex)->second.insert(target_vertex);
+		}
+		else {
+			throw invalid_argument("Vertex not found in the Graph.");
+		}
+	}
+
+	void RemoveEdge(int source_vertex, int target_vertex) {
+		if (adjacency_list_.count(source_vertex)) {
+			adjacency_list_.find(source_vertex)->second.erase(target_vertex);
+		}
+	}
+
+};
