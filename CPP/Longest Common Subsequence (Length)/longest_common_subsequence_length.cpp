2eef9fb68edfca095efec2e6bc161a98d1da71a1
Aman Mehara <amanmehara@hotmail.com>
2018-03-18 11:59:17 +0530

    Add 1 Project
    CPP ## Longest Common Subsequence (Length)

---
+++ CPP/Longest Common Subsequence (Length)/longest_common_subsequence_length.cpp
@@ -0,0 +1,41 @@
+/*
+* Copyright 2018 Aman Mehara
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+#include"longest_common_subsequence_length.h"
+
+#include <algorithm>
+#include <vector>
+
+int LongestCommonSubsequenceLength(std::vector<char> sequence1, std::vector<char> sequence2) {
+
+    size_t sequence1_size = sequence1.size();
+    size_t sequence2_size = sequence2.size();
+
+    std::vector<std::vector<char>> table(sequence1_size + 1, std::vector<char>(sequence2_size + 1, 0));
+
+    for(size_t index1 = 0; index1 < sequence1_size; index1++) {
+        for(size_t index2 = 0; index2 < sequence2_size; index2++) {
+            if(sequence1[index1] == sequence2[index2]) {
+                table[index1 + 1][index2 + 1] = 1 + table[index1][index2];
+            }
+            else {
+                table[index1 + 1][index2 + 1] = std::max(table[index1][index2 + 1], table[index1 + 1][index2]);
+            }
+        }
+    }
+
+    return table[sequence1_size][sequence2_size];
+
+}
