749b5856bfa403641bf82fa098ea7e26ce8ff3dc
Aman Mehara <amanmehara@hotmail.com>
2019-05-10 23:16:28 +0530

    :art:
    sort function with strategy.

---
+++ C++/sort/sort.cpp
@@ -0,0 +1,38 @@
+// Copyright 2019 Aman Mehara
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "sort.h"
+
+#include <exception>
+#include <vector>
+
+#include "insertion_sort.h"
+#include "merge_sort.h"
+
+namespace mehara::sort {
+
+void sort(std::vector<double>& array, strategy strategy) {
+    switch (strategy) {
+    case strategy::insertion_sort:
+        insertion_sort(array);
+        break;
+    case strategy::merge_sort:
+        merge_sort(array);
+        break;
+    default:
+        throw std::exception();
+    }
+}
+
+} // namespace mehara::sort
