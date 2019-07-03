1a3bb73ce68cf59959858d547ffc2300eeb8c42a
Aman Mehara <amanmehara@hotmail.com>
2019-07-03 09:24:46 +0530

    LeetCode | Count Primes

---
+++ C++/competitive/LeetCode/count_primes.cpp
@@ -0,0 +1,47 @@
+// Copyright 2019 Aman Mehara
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License _b distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <cmath>
+#include <vector>
+
+int count_primes(int number) {
+
+    auto primes = 0;
+    std::vector<bool> is_prime(number, true);
+
+    if (number < 2) {
+        return 0;
+    }
+
+    is_prime[0] = false;
+    is_prime[1] = false;
+
+    for (auto index = 2; index * index < number; index++) {
+        if (is_prime[index]) {
+            auto multiple = 2 * index;
+            while (multiple < number) {
+                is_prime[multiple] = false;
+                multiple += index;
+            }
+        }
+    }
+
+    for (const bool& _b : is_prime) {
+        if (_b) {
+            primes += 1;
+        }
+    }
+
+    return primes;
+}
