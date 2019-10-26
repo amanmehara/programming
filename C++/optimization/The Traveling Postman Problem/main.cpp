43d344e93ce2558992b3c8c1d19c0e6cb9433e6d
Rafael Castro <rafaelgyncastro63@gmail.com>
2019-10-26 18:03:48 -0300

    Traveling Postman Problem

--- C++/optimization/The Traveling Postman Problem/main.cpp
+++ C++/optimization/The Traveling Postman Problem/main.cpp
@@ -5,7 +5,7 @@
 int main(){
 	TheTravelingPostmanProblem test({{0, 1}, {1, 0}, {2, 2}, {3, 3}}, {0, 0});
 
-	printf("%d\n", test.MinimumDistance());
+	printf("The MinimumDistance to take the points is %d\n", test.MinimumDistance());
 
 	return 0;
 }
