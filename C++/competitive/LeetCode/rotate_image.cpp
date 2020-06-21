460f2e21489d3753cfba7688602ea8e00aabada4
Aman Mehara <amanmehara@hotmail.com>
2020-06-21 22:22:29 +0530

    LeetCode | Rotate Image

---
+++ C++/competitive/LeetCode/rotate_image.cpp
@@ -0,0 +1,29 @@
+// Copyright 2020 Aman Mehara
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <vector>
+
+void rotate(std::vector<std::vector<int>>& matrix)
+{
+    int n = matrix.size();
+    for (int i = 0; i < n / 2; i++) {
+        for (int j = i; j < n - 1 - i; j++) {
+            int t = matrix[i][j];
+            matrix[i][j] = matrix[n - 1 - j][i];
+            matrix[n - 1 - j][i] = matrix[n - 1 - i][n - 1 - j];
+            matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+            matrix[j][n - 1 - i] = t;
+        }
+    }
+}
