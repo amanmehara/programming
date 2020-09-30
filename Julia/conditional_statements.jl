3a291d7804eada3687eaab87bded7b6f9a8dc6b1
Navendu Pottekkat <navendu@localhost.localdomain>
2020-09-30 21:28:30 +0530

    add julia language

---
+++ Julia/conditional_statements.jl
@@ -0,0 +1,7 @@
+# Conditional statements in Julia
+
+if true
+    println("It's true!")
+else
+    println("It's false!")
+end
---
+++ Julia/for_loops.jl
@@ -0,0 +1,5 @@
+# for loops in Julia
+
+for i in 1:5
+    print(i, ", ")
+end
