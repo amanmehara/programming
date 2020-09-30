5b1fa9ad24bb1c7c11924d3db4fc992583450ee1
Navendu Pottekkat <navendu@localhost.localdomain>
2020-09-30 21:28:49 +0530

    hello world and functions

---
+++ Julia/functions.jl
@@ -0,0 +1,7 @@
+# Functions in Julia
+
+function vol_sphere(r)
+    # Julia allows Unicode names (in UTF-8 encoding)
+    # Either "pi" or the symbol π can be used
+    return 4/3*pi*r^3
+end
---
+++ Julia/hello_world.jl
@@ -0,0 +1,3 @@
+# Hello World! in Julia
+
+println("Hello World!")
