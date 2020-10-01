b0f02e155c78c7764f0c3f9842bdb246a6cb3abf
Navendu Pottekkat <navendupottekkat@gmail.com>
2020-10-01 12:36:08 +0530

    [Julia] Hello World & Syntax (#85)

---
+++ Julia/Conditional Statements/conditional_statements.jl
@@ -0,0 +1,9 @@
+# Conditional statements in Julia
+# Navendu Pottekkat | 2020 | Apache-2.0 License
+# https://www.apache.org/licenses/LICENSE-2.0
+
+if true
+    println("It's true!")
+else
+    println("It's false!")
+end
