bafb638b0838f2ff049c7d9005438efcf08e5e0d
Anjali Soni <anjalisony32@gmail.com>
2020-10-01 13:28:10 +0530

    Update auto complete.cpp

--- C++/trie/auto complete.cpp
+++ C++/trie/auto complete.cpp
@@ -1,12 +1,21 @@
-#include <string>
-#include <vector>
-#include <iostream>
-#include <string>
+
+// Copyright 2020 Anjali Soni
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <bits/stdc++.h>
 using namespace std;
 
-// #include "TrieNode.h"
-#include <string>
-#include <vector>
 class TrieNode
 {
 public:
@@ -80,11 +89,6 @@ public:
             this->count++;
         }
     }
-
-    //      void search(string word) {
-
-    //         searchWord(root , word , word);
-    //     }
 
     void print(TrieNode *root, string s)
     {
