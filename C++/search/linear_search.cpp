6af19b41182c00f1c45f89d831634d58cf9d3644
Kartikey Sharma <36632816+KartikeySharma@users.noreply.github.com>
2020-10-01 13:00:13 +0530

    Update linear_search.cpp
    Updated the Apache license according to the Contributers.md file

--- C++/search/linear_search.cpp
+++ C++/search/linear_search.cpp
@@ -1,3 +1,19 @@
+/*
+Copyright [2020] [Kartikey Sharma]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -6,6 +22,8 @@ int search(vector<int> v, int find)
 	int l= v.size();
 	for(int i=0; i<l ; i++)
 	{
+		//return the position of the element if found
+		//for multiple occurences, it will only return the position of the first occurence
 		if(find==v[i])
 			return i;
 	}
@@ -33,3 +51,5 @@ int main()
 		
 	return 0;
 }
+
+
