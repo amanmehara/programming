a0ccebd82f4cdb8191bd16c713e4cf2f12341b97
Anoop Bhat <anoopbhat47@gmail.com>
2020-10-02 16:24:19 +0530

    modified changes required

--- Python/Algorithms/Dijkstras.py
+++ Python/Algorithms/Dijkstras.py
@@ -1,20 +1,33 @@
- 
+'''
+Copyright [2020] [Anoop Bhat]
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
+
+
 import math
 
 def Dijkstras(graph, start, goal):
-    unseenNodes = graph
+    unseen_nodes = graph
     predecessor = {}
     shortest_distance = {}
     path = []
     infinty = math.inf
 
-    for node in unseenNodes:
+    for node in unseen_nodes:
         shortest_distance[node] = infinty
     shortest_distance[start] = 0
 
-    while unseenNodes:
+    while unseen_nodes:
         minNode = None
-        for node in unseenNodes:
+        for node in unseen_nodes:
             if minNode is None:
                 minNode = node
 
@@ -26,15 +39,15 @@ def Dijkstras(graph, start, goal):
                 shortest_distance[chilNode] = weight + shortest_distance[minNode]
                 predecessor[chilNode] = minNode
         
-        unseenNodes.pop(minNode)
+        unseen_nodes.pop(minNode)
 
     
-    currenNode = goal
-    while currenNode != start:
+    curren_node = goal
+    while curren_node != start:
         try:
-            path.insert(0,currenNode)
+            path.insert(0,curren_node)
 
-            currenNode = predecessor[currenNode]
+            curren_node = predecessor[curren_node]
         except KeyError:
             print('path not found')
             break
@@ -48,3 +61,5 @@ def Dijkstras(graph, start, goal):
 
 graph = {'a':{'b':10,'c':3},'b':{'c':1,'d':2},'c':{'b':4,'d':8,'e':2},'d':{'e':7},'e':{'d':9}}
 Dijkstras(graph, 'a', 'd')
+
+
--- Python/Algorithms/quicksort.py
+++ Python/Algorithms/quicksort.py
@@ -1,5 +1,17 @@
+'''
+Copyright [2020] [Anoop Bhat]
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+'''
 
-def quickSort(sequence):
+def quick_sort(sequence):
 
     lenght = len(sequence)
     if lenght <= 1:
@@ -17,6 +29,8 @@ def quickSort(sequence):
             items_larger.append(item)
 
     
-    return quickSort(items_lower) + [pivot] + quickSort(items_larger)
+    return quick_sort(items_lower) + [pivot] + quick_sort(items_larger)
 
-print(quickSort([5,4,2,1,7,9,3,1]))
+print(quick_sort([5,4,2,1,7,9,3,1]))
+
+
