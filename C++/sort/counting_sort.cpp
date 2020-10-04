e0aec2873424aac1e45e01e0923a8603412eaedf
2018A7PS0093G <f20180093@goa.bits-pilani.ac.in>
2020-10-04 13:13:29 +0530

    Changes to countSort

--- C++/sort/counting_sort.cpp
+++ C++/sort/counting_sort.cpp
@@ -1,51 +1,50 @@
-// C++ Program for counting sort 
+// Copyright 2020 Nilay Rajderkar
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#include "counting_sort.h"
+
 #include <bits/stdc++.h> 
 #include <string.h> 
 using namespace std; 
-#define RANGE 255 
+#define LIMIT 255 
 
-// The main function that sort 
-// the given string arr[] in 
-// alphabatical order 
+//sorting the given string in alphabetical order
 void countSort(char arr[]) 
 { 
-	// The output character array 
-	// that will have sorted arr 
+	//the array having sorted characters
 	char output[strlen(arr)]; 
 
-	// Create a count array to store count of inidividul 
-	// characters and initialize count array as 0 
-	int count[RANGE + 1], i; 
+	// array to count individual characters
+	int count[LIMIT + 1], i; 
 	memset(count, 0, sizeof(count)); 
 
-	// Store count of each character 
+	//to store count
 	for (i = 0; arr[i]; ++i) 
 		++count[arr[i]]; 
 
 	// Change count[i] so that count[i] now contains actual 
 	// position of this character in output array 
-	for (i = 1; i <= RANGE; ++i) 
+	for (i = 1; i <= LIMIT; ++i) 
 		count[i] += count[i - 1]; 
 
-	// Build the output character array 
+	// building the output array
 	for (i = 0; arr[i]; ++i) { 
 		output[count[arr[i]] - 1] = arr[i]; 
 		--count[arr[i]]; 
 	} 
 
-	/* 
-	For Stable algorithm 
-	for (i = sizeof(arr)-1; i>=0; --i) 
-	{ 
-		output[count[arr[i]]-1] = arr[i]; 
-		--count[arr[i]]; 
-	} 
-	
-	For Logic : See implementation 
-	*/
-
-	// Copy the output array to arr, so that arr now 
-	// contains sorted characters 
 	for (i = 0; arr[i]; ++i) 
 		arr[i] = output[i]; 
 } 
@@ -53,12 +52,16 @@ void countSort(char arr[])
 
 int main() 
 { 
-	char arr[] = "geeksforgeeks"; 
+	char arr[] = "thisisabrowncow"; 
 
 	countSort(arr); 
 
-	cout << "Sorted character array is " << arr; 
+	cout << "The final sorted array is" << arr; 
 	return 0; 
-} 
+}  //end of code 
 
-// This code is contributed by rathbhupendra 
+
+
+
+
+
