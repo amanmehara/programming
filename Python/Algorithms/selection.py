a99139331da2a15234ec95a1c8fb68a7a51e5c52
priyanka prasad <59612128+P-riyanka-prasad@users.noreply.github.com>
2020-10-05 01:13:13 +0530

    Create selection.py

---
+++ Python/Algorithms/selection.py
@@ -0,0 +1,21 @@
+def selectionSort(array):
+  n = len(array)
+  for i in range(n):
+    # Initially, assume the first element of the unsorted part as the minimum.
+    minimum = i
+
+    for j in range(i+1, n):
+      if (array[j] < array[minimum]):
+        # Update position of minimum element if a smaller element is found.
+        minimum = j
+
+    # Swap the minimum element with the first element of the unsorted part.     
+    temp = array[i]
+    array[i] = array[minimum]
+    array[minimum] = temp
+    
+  return array
+
+# Driver code
+array = [13, 4, 9, 5, 3, 16, 12]
+print(selectionSort(array))
