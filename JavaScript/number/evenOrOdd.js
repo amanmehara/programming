b1f4806aea1adbd5a31cc2059727a87139624988
Angelo Koddi <angelo.koddi@gmail.com>
2020-10-07 10:42:54 +0800

    add number folder to javascript and add a number algorithm

---
+++ JavaScript/number/evenOrOdd.js
@@ -0,0 +1,21 @@
+//  this algorithm will check if the number is even or not
+const CheckEvenOdd = (num) => {
+  //  this will check if the input is a number or not
+  if (typeof num !== "number") {
+    return "Not a Number";
+  }
+  //    this will check if the input number is a whole number or not
+  if (num !== Math.floor(num)) {
+    return "Not a Whole Number";
+  }
+  //if the input number is a whole number
+  //we're gonna use modulo operator to know if the number is even or not
+  //modulo operator - arithmetic operator produced the reminder of an integer division
+  //if the reminder of num % 2 is equal to zero the number is even else it is odd
+  return num % 2 === 0 ? "Even" : "Odd";
+};
+console.log(CheckEvenOdd(2.2));
+console.log(CheckEvenOdd("even"));
+console.log(CheckEvenOdd(100));
+console.log(CheckEvenOdd(9));
+console.log(CheckEvenOdd(0));
