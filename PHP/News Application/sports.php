439f3f96d6a878296d8e34911fc104828b3d9eca
Sovam-coder <sovam121@gmail.com>
2020-10-13 23:54:07 +0530

    Create a new application using Php

---
+++ PHP/News Application/sports.php
@@ -0,0 +1,294 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hindustan Khabri</title>
+    <style>
+        .jumbotron
+        {
+            background-image: linear-gradient(to right, rgb(155, 7, 253) , rgb(236, 14, 118));
+        }
+        
+        .cards {
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  text-decoration: none;
+}
+.card-box {
+    position: relative;
+    color: #fff;
+    padding: 20px 10px 40px;
+    margin: 20px 0px;
+    text-decoration: none;
+}
+.card-box:hover {
+    text-decoration: none;
+    color: #f1f1f1;
+    text-decoration: none;
+}
+.card-box:hover .icon i {
+    font-size: 100px;
+    transition: 1s;
+    -webkit-transition: 1s;
+    text-decoration: none;
+}
+.card-box .inner {
+    padding: 5px 10px 0 10px;
+    text-decoration: none;
+}
+.card-box h3 {
+    font-size: 27px;
+    font-weight: bold;
+    margin: 0 0 8px 0;
+    white-space: nowrap;
+    padding: 0;
+    text-align: left;
+    text-decoration: none;
+}
+.
+.card-box p {
+    font-size: 15px;
+    text-decoration: none;
+}
+.card-box .icon {
+    position: absolute;
+    top: auto;
+    bottom: 5px;
+    right: 5px;
+    z-index: 0;
+    font-size: 72px;
+    color: rgba(0, 0, 0, 0.15);
+}
+.card-box .card-box-footer {
+    position: absolute;
+    left: 0px;
+    bottom: 0px;
+    text-align: center;
+    padding: 3px 0;
+    color: rgba(255, 255, 255, 0.8);
+    background: rgba(0, 0, 0, 0.1);
+    width: 100%;
+    text-decoration: none;
+}
+.card-box:hover .card-box-footer {
+    background: rgba(0, 0, 0, 0.3);
+}
+.bg-top {
+    background-image: url(images/8.jpg);
+}
+ .bg-sport {
+    background-image: url(images/2.png);
+}
+.bg-science {
+    background-image: url(images/7.jpg);
+}
+.bg-tech {
+    background-image: url(images/3.jpg);
+}
+.bg-health {
+    background-image: url(images/6.jpg);
+}
+ .bg-enter {
+    background-image: url(images/5.jpg);
+}
+.bg-business {
+    background-image: url(images/1.jpg);
+}
+.bg-bitcoin{
+    background-image: url(images/4.jpg);
+}
+    </style>
+  </head>
+  <body >
+    <div class="jumbotron">
+
+        <strong><center><h1 style="color: white;">Hindustan Khabri</h1></center></strong>
+        </div>
+
+        <div class="container-fluid">
+        
+            <div class="row">
+            
+                <div class="col-lg-3 col-sm-6">
+                <a href="index.php">
+                    <div class="card-box bg-top">
+                        <div class="inner">
+                            <br>
+                            <center><h1>Headlines </h1></center>
+                           
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                        </div>
+                       
+                    </div>
+                </div>
+            </a>
+                <div class="col-lg-3 col-sm-6">
+                <a href="sports.php">
+                    <div class="card-box bg-sport">
+                        <div class="inner">
+                            <br>
+                            <center><h1>Sports</h1></center>
+                           
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                        </div>
+                       
+                    </div>
+                </div>
+            </a>    
+                <div class="col-lg-3 col-sm-6">
+                <a href="science.php">
+                    <div class="card-box bg-science">
+                        <div class="inner">
+                            <br>
+                            <center><h1>Science</h1></center>
+                           
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                        </div>
+                       
+                    </div>
+                </div>
+            </a>
+                <div class="col-lg-3 col-sm-6">
+                <a href="technology.php">
+                    <div class="card-box bg-tech">
+                        <div class="inner">
+                            <br>
+                            <center><h1>Technology</h1></center>
+                           
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                        </div>
+                       
+                    </div>
+                </div>
+            </a>
+        <div class="container-fluid">
+        
+            <div class="row">
+                <div class="col-lg-3 col-sm-6">
+                <a href="health.php">
+                    <div class="card-box bg-health">
+                        <div class="inner">
+                            <br>
+                            <center><h1>Health</h1></center>
+                           
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                        </div>
+                       
+                    </div>
+                </div>
+            </a>
+                <div class="col-lg-3 col-sm-6">
+                <a href="entertainment.php">
+                    <div class="card-box bg-enter">
+                        <div class="inner">
+                            <br>
+                            <center><h1>Entertainment</h1></center>
+                           
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                        </div>
+                       
+                    </div>
+                </div>
+            </a>
+                <div class="col-lg-3 col-sm-6">
+                <a href="business.php">
+                    <div class="card-box bg-business">
+                        <div class="inner">
+                            <br>
+                            <center><h1>Business</h1></center>
+                           
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                        </div>
+                       
+                    </div>
+                </div>
+                </a>
+                <div class="col-lg-3 col-sm-6">
+                <a href="bitcoin.php">
+                    <div class="card-box bg-bitcoin">
+                        <div class="inner">
+                            <br>
+                            <center><h1>BitCoin</h1></center>
+                           
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-graduation-cap" aria-hidden="true"></i>
+                        </div>
+                       
+                    </div>
+                </div>
+</a>
+            </div>
+        </div>
+
+        <?php
+        $url='http://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=d702491025274dcfb46aca37f6db3056';
+        $response= file_get_contents($url);
+        $NewsData= json_decode($response);
+?>
+     
+<div class='container'>
+    <center><h1 style="color:red;">Top Sports News</h1></center>
+    <hr>
+    <br>
+<?php
+  foreach($NewsData->articles as $pa)
+  {
+?>
+<div class="card" >
+<div class='row'>
+    
+    <div class='col-md-3'>
+        <br>
+    <img src="<?php echo $pa->urlToImage  ?>" alt="" width="100%">
+    </div>
+    <div class='col-md-9'>
+    <h4 > <?php echo $pa->title    ?> </h4>
+    <hr>
+    <h6> <?php echo $pa->description  ?></h6>
+    <p><?php echo $pa->content ?></p>
+     &nbsp;<a href="<?php echo   $pa->url  ?>">See More</a>
+    <h6>Author: <?php echo $pa->author ?></h6>
+    <h6>Published:<?php echo  $pa->publishedAt ?></h6>
+   
+    </div>
+  </div>
+</div>
+<br>
+
+<?php
+  }
+  ?>
+  <hr>
+</div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
