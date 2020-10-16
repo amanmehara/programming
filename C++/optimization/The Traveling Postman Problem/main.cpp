c1c6600938ebcceec33d092133195ae22531fc45
@RafaelCS <rafaelgyncastro63@gmail.com>
2020-10-16 02:32:17 -0300

    [C++] Travelling Salesman Problem (#139)

--- C++/optimization/The Traveling Postman Problem/main.cpp
+++ C++/optimization/The Traveling Postman Problem/main.cpp
@@ -3,9 +3,20 @@
 #include "the_traveling_postman_problem.h"
 
 int main(){
-	TheTravelingPostmanProblem test({{0, 1}, {1, 0}, {2, 2}, {3, 3}}, {0, 0});
+ TheTravelingPostmanProblem test({{0, 1}, {1, 0}, {2, 2}, {3, 3}}, {0, 0});
+ auto ans = test.minimum_distance();
 
-	printf("The MinimumDistance to take the points is %d\n", test.MinimumDistance());
+ printf("The Minimum Distance to take the points is %d\n", ans.first);
 
-	return 0;
+ printf("The path: ");
+
+ printf("(%d, %d) -> ", test.start.x, test.start.y);
+
+ for(int i = 0 ; i < ans.second.size() ; i++){
+  printf("(%d, %d) -> ", ans.second[i].x, ans.second[i].y);
+ }
+
+ printf("(%d, %d)\n", test.start.x, test.start.y);
+
+ return 0;
 }
