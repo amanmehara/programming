18433fd7eab41ed417cbf88e887f5427b3886778
Suyash Patel <59373438+iamsuyashpatel07@users.noreply.github.com>
2020-10-17 17:01:59 +0530

    Added name
    Changed name

--- C++/sort/MergeTwoSortedArray.cpp
+++ C++/sort/MergeTwoSortedArray.cpp
@@ -1,5 +1,5 @@
   
-// Copyright 2019 Aman Mehara
+// Copyright 2019 Suyash Patel
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
