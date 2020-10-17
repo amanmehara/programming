151d0f9835668fcae950f04d1835a07b365acc6d
Raswanth Rajangam <59132948+Raswanth8@users.noreply.github.com>
2020-10-17 17:12:32 +0530

    Update interpolation search.py

--- Python/Algorithms/interpolation search.py
+++ Python/Algorithms/interpolation search.py
@@ -1,14 +1,32 @@
+'''Copyright [2020] [Raswanth]
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+    
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.'''
+
+# Program to implement interpolation search algorithm in Python3 language.
+
+# function to perform interpolation search
 def intpolsearch(values,x ):
     idx0 = 0
     idxn = (len(values) - 1)
 
     while idx0 <= idxn and x >= values[idx0] and x <= values[idxn]:
 
+# Find the mid point
 	mid = idx0 +\
                int(((float(idxn - idx0)/( values[idxn] - values[idx0]))
                     * ( x - values[idx0])))
 
-
+# Compare the value at mid point with search value 
         if values[mid] == x:
             return "Found "+str(x)+" at index "+str(mid)
 
@@ -17,5 +35,5 @@ def intpolsearch(values,x ):
     return "Searched element not in the list"
 
 
-l = [2, 6, 11, 18, 27, 31, 45, 120]
+l = [2, 6, 11, 19, 27, 32, 45, 120]
 print(intpolsearch(l, 2))
