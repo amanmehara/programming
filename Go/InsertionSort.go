9b64409f05e719a9657e2b891e3d9b519da29433
Anil Kumar Teegala <34655912+AnilKumarTeegala@users.noreply.github.com>
2020-10-17 11:36:44 +0530

    Create InsertionSort.go

---
+++ Go/InsertionSort.go
@@ -0,0 +1,53 @@
+/*Copyright 2020 Anil Kumar Teegala
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+    
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Code for Insertion Sort in Go
+
+package main
+
+import (
+    "fmt"
+    "math/rand"
+    "time"
+)
+
+func main() {
+
+    slice := generateSlice(20)
+    fmt.Println("\n--- Unsorted --- \n\n", slice)
+    insertionsort(slice)
+    fmt.Println("\n--- Sorted ---\n\n", slice, "\n")
+}
+
+func generateSlice(size int) []int {
+
+    slice := make([]int, size, size)
+    rand.Seed(time.Now().UnixNano())
+    for i := 0; i < size; i++ {
+        slice[i] = rand.Intn(999) - rand.Intn(999)
+    }
+    return slice
+}
+
+func insertionsort(items []int) {
+    var n = len(items)
+    for i := 1; i < n; i++ {
+        j := i
+        for j > 0 {
+            if items[j-1] > items[j] {
+                items[j-1], items[j] = items[j], items[j-1]
+            }
+            j = j - 1
+        }
+    }
+}
