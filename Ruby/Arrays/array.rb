5e754067aa7181e050818c788591524d5eeeab8c
Sabarish Rajamohan <sabarishrajamohan@gmail.com>
2020-10-21 02:45:13 +0530

    Added Ruby materials
    1. Array
    2. Hash
    3. Times keyword
    4. Unless keyword

---
+++ Ruby/Arrays/array.rb
@@ -0,0 +1,23 @@
+# Copyright 2020 sabarishrajamohan@gmail.com
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+new_array = [1,2,3,4]
+
+new_string_array = ["hello", "world"]
+
+# Methods
+
+new_array.length # gives the length
+new_array.first # gives the first element
+new_array.last # gives last element
+
+# For other array methods, go to irb console and,
+Array.methods
---
+++ Ruby/Hashes/hash.rb
@@ -0,0 +1,23 @@
+# Copyright 2020 sabarishrajamohan@gmail.com
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# A hash is like a Dictionary data stucturein Python. It consists of key value pairs
+
+days = Hash.new
+days = {"1" => "Sunday", "2" => "Monday"}
+
+# here "1" and "2" are keys and the days are corresponding values to the keys. You can access them like
+
+days["1"] or days["2"]
+
+# To list out all the keys, use
+
+days.keys
