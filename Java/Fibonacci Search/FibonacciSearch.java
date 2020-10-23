4bb3d532c27f3159c18fc8c95f5e68cf243a2f19
sumitaccess007 <sumitaccess007@gmail.com>
2020-10-23 10:38:37 +0530

    Implementation of Fibonacci Search in Java (Issue #80)

--- Java/Fibonacci Search/FibonacciSearch.java
+++ Java/Fibonacci Search/FibonacciSearch.java
@@ -1,6 +1,6 @@
 package Search;
 
-/* Copyright 2018 Aman Mehara
+/* Copyright 2020 Sumit Sharma
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
