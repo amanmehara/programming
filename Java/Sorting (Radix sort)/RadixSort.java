4817c9b73ba3d2259e5266872c99afda6f77da40
Sanjana Pedada <sanjana.pedada@publicissapient.com>
2020-10-27 12:07:14 +0530

    Add Radix Sort Sorting algorithm

---
+++ Java/Sorting (Radix sort)/RadixSort.java
@@ -0,0 +1,64 @@
+/*
+ * Radix sort is to do digit by digit sort starting from least significant digit 
+ * to most significant digit using counting sort as a subroutine to sort.
+ * 
+ */
+
+import java.util.*;
+
+public class RadixSort {
+
+    /**
+     * Function to sort the arr[] list of length n using radix sort
+     * 
+     * @param arr The array list that is to be sorted
+     * @param a   The length of the array
+     */
+    private static void radixsort(int arr[], int a) {
+        int max = arr[0];
+        for (int i = 0; i < a; i++) {
+            if (arr[i] > max)
+                max = arr[i];
+        }
+
+        for (int j = 1; max / j > 0; j *= 10)
+            countSort(arr, a, j);
+    }
+
+    /**
+     * Sorts the elements of arr[] according to the digit represented by j
+     * 
+     * @param arr The array list
+     * @param a   The length of the array
+     * @param j   10^i where i is the current digit number
+     */
+    private static void countSort(int arr[], int a, int j) {
+        int result[] = new int[a];
+        int i;
+        int count[] = new int[10];
+        Arrays.fill(count, 0);
+
+        for (i = 0; i < a; i++)
+            count[(arr[i] / j) % 10]++;
+
+        for (i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+
+        for (i = a - 1; i >= 0; i--) {
+            result[count[(arr[i] / j) % 10] - 1] = arr[i];
+            count[(arr[i] / j) % 10]--;
+        }
+
+        for (i = 0; i < a; i++)
+            arr[i] = result[i];
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 20, 399, 70, 9, 78, 80, 32, 500, 499 };
+        int a = arr.length;
+        System.out.println("Testing Radix Sort");
+        System.out.println("Before Sort: " + Arrays.toString(arr));
+        radixsort(arr, a);
+        System.out.println("After Sort: " + Arrays.toString(arr));
+    }
+}
